20356e69f3265fbc11a48fc51b0648ec8379442d
Katherine Hambley <khambley@franklinenergy.com>
2021-01-01 11:00:15 -0600

    Added image upload and display functionality to Parks

--- MyThrillRideTrackerApp3/Controllers/ParksController.cs
+++ MyThrillRideTrackerApp3/Controllers/ParksController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using MyThrillRideTrackerApp3.Models;
 
 namespace MyThrillRideTrackerApp3.Controllers
@@ -21,7 +24,11 @@ namespace MyThrillRideTrackerApp3.Controllers
         // GET: Parks
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Parks.ToListAsync());
+            // Can't use ToListAsync with var or List<Park> -> must be IEnumerable<Park> or IQueryable<Park>
+            // because they implement the GetEnumerator iterator method 12/28/20 KLH
+            IEnumerable<Park> parksList = await _context.Parks.Include(p => p.ImageFiles).ToListAsync();
+
+            return View(parksList);
         }
 
         // GET: Parks/Details/5
@@ -53,12 +60,17 @@ namespace MyThrillRideTrackerApp3.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ParkId,Name,Description,City,State,WebsiteLink,ParkMapLink")] Park park)
+        public async Task<IActionResult> Create([Bind("ParkId,Name,Description,City,State,WebsiteLink,ParkMapLink")] Park park, List<IFormFile> files)
         {
             if (ModelState.IsValid)
             {
+                // 1. Save the park model first, creates a unique id for the inserted park.
                 _context.Add(park);
                 await _context.SaveChangesAsync();
+
+                // 2. Save the ImageFiles in Images folder and FileName in db.
+                await SaveParkImageFiles(park, files);
+
                 return RedirectToAction(nameof(Index));
             }
             return View(park);
@@ -148,5 +160,46 @@ namespace MyThrillRideTrackerApp3.Controllers
         {
             return _context.Parks.Any(e => e.ParkId == id);
         }
+        private async Task SaveParkImageFiles(Park park, List<IFormFile> files)
+        {
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    if (file.Length > 0)
+                    {
+                        //Getting FileName
+                        var fileName = Path.GetFileName(file.FileName);
+
+                        //Assigning Unique Filename (Guid)
+                        var myUniqueFileName = Convert.ToString(Guid.NewGuid());
+
+                        //Getting file Extension
+                        var fileExtension = Path.GetExtension(fileName);
+
+                        // concatenating  FileName + FileExtension
+                        var newFileName = String.Concat(myUniqueFileName, fileExtension);
+
+                        // Combines two strings into a path.
+                        var filepath =
+                            new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Images")).Root + $@"\{newFileName}";
+
+                        using (FileStream fs = System.IO.File.Create(filepath))
+                        {
+                            file.CopyTo(fs);
+                            fs.Flush();
+                        }
+
+                        var imageFileName = new ImageFileName()
+                        {
+                            FileName = newFileName,
+                            ParkId = park.ParkId
+                        };
+                        _context.ImageFileNames.Add(imageFileName);
+                        await _context.SaveChangesAsync();
+                    }
+                }
+            }
+        } // end SaveImageFiles
     }
 }
